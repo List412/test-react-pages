00836a045dfbf1c91552c43f52c7f23a91174840
Evgeniy Burlakov <evgeniy.burlakov@lenta.com>
2022-08-07 17:37:00 +0500

    dd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          пуп <code>вот вроде работает</code>ыыпаыаыаы
+          пуп <code>вот вроде работает</code>ыыпаыаыаыkfdfdfghggh
         </p>
         <a
           className="App-link"
